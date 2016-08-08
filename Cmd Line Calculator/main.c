f07bc43924b1b4a39dbd4a785d40ab05ecb99251
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-08 13:27:50 -0400

    Initial Commit

---
+++ Cmd Line Calculator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Cmd Line Calculator
+//
+//  Created by Dan Esrey on 2016/8/8.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
