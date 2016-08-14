14e634b48bdeffd90e2efaa3db559f506c33f8df
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-14 17:34:03 -0400

    adding error handling to initial calc

--- Cmd Line Calculator/main.c
+++ Cmd Line Calculator/main.c
@@ -11,9 +11,8 @@
 int main(int argc, const char * argv[]) {
     
     printf("DAN'S COMMAND LINE CALCULATOR\n\n");
-           
+    
     printf("Which operation would you like to perform? Type 1 for addition, type 2 for subtraction, type 3 for multiplication, or type 4 for division: \n\n");
-    // this prompts user to select operation
     
     int a = -1;
     int error = -1;
@@ -24,26 +23,38 @@ int main(int argc, const char * argv[]) {
         if (error != 1 || a < 0 || a >= 5 ) {
             printf("please enter 1, 2, 3, or 4: \n\n");
             a = -1;
-    // this prompts the user to enter 1, 2, 3, or 4 if the user enters an invalid selection
         }
+    }
+    printf("\nWhat is the first number of your operation? \n\n");
     
-        printf("\nWhat is the first number of your operation? \n\n");
-    // this asks for the number to which you are adding, from which you are subtracting, or which you are multiplying
-        
-        double firstNumber = 7;
-        double secondNumber = 8;
-        
-        scanf("%lf", &firstNumber);
-        printf("\nWhat is the second number of your operation? \n\n");
-    // this asks for the number to be added to, subtracted from, or multiplied by the first number
-        
-        scanf("%lf", &secondNumber); {
+    double firstNumber = 10000;
+    while (firstNumber < -9999 || firstNumber > 9999) {
+        fpurge(stdin);
+        error = scanf("%lf", &firstNumber);
+        if (error != 1 || firstNumber < -9999 || firstNumber > 9999) {
+            printf("Please enter a valid number");
+            firstNumber = 10000;
+        }
+    }
+    printf("\nWhat is the second number of your operation? \n\n");
+    
+    double secondNumber = 8;
+    while (secondNumber < -9999 || secondNumber > 9999) {
+        fpurge(stdin);
+        error = scanf("%lf", &secondNumber);
+        if (error != 1 || secondNumber < -9999 || secondNumber > 9999) {
+            printf("Please enter a valid number");
+            secondNumber = 10000;
+        }
+    }
+ 
+    scanf("%lf", &secondNumber); {
         
         if (a == 1) {
             double result = firstNumber + secondNumber;
             printf("\nThe sum is %lf\n\n", result);
         }
-    
+        
         if (a == 2) {
             double result = firstNumber - secondNumber;
             printf("\nThe difference is %lf\n\n", result);
@@ -53,83 +64,80 @@ int main(int argc, const char * argv[]) {
             double result = firstNumber * secondNumber;
             printf("\nThe product is %lf\n\n", result);
         }
-            if (a == 4) {
-                if (secondNumber == 0) {
-                    printf("\nI'm sorry, but you cannot divide by zero!\n\n");}
-                else
-                {
+        if (a == 4) {
+            if (secondNumber == 0) {
+                printf("\nI'm sorry, but you cannot divide by zero!\n\n");}
+            else
+            {
                 double result = firstNumber / secondNumber;
-                    printf("\nThe quotient is %lf\n\n", result);}
-            }
+                printf("\nThe quotient is %lf\n\n", result);}
         }
-            printf("Would you like DAN'S COMMAND LINE CALCULATOR to perform another calculation?(1=Y/2=N)\n\n");
-        int anotherCalc = -1;
-        int Mistake = -1;
+    }
+    printf("Would you like DAN'S COMMAND LINE CALCULATOR to perform another calculation?(1=Y/2=N)\n\n");
+    int anotherCalc = -1;
+    int Mistake = -1;
+    
+    while (anotherCalc < 1 || anotherCalc > 2) {
+        fpurge(stdin);
+        Mistake = scanf("%d", &anotherCalc);
+        if (Mistake != 1 || anotherCalc < 1 || anotherCalc > 2) {
+            printf("Please enter 1 for yes or 2 for no: \n\n");
+            anotherCalc = -1;
+        }
+    }
+    while (anotherCalc == 1) {
         
-            while (anotherCalc < 1 || anotherCalc > 2) {
-                fpurge(stdin);
-                Mistake = scanf("%d", &anotherCalc);
-                if (Mistake != 1 || anotherCalc < 1 || anotherCalc > 2) {
-                    printf("Please enter 1 for yes or 2 for no: \n\n");
-                    anotherCalc = -1;
+        printf("\nWhich operation would you like to perform? Type 1 for addition, type 2 for subtraction, type 3 for multiplication, or type 4 for division: \n\n");
+        
+        int a = -1;
+        int error = -1;
+        
+        while (a < 0 || a >= 5) {
+            fpurge(stdin);
+            error = scanf("%d", &a);
+            if (error != 1 || a < 0 || a >= 5 ) {
+                printf("please enter 1, 2, 3, or 4: \n\n");
+                a = -1;
+            }
+            
+            printf("\nWhat is the first number of your operation? \n\n");
+            
+            double firstNumber = 7;
+            double secondNumber = 8;
+            
+            scanf("%lf", &firstNumber);
+            printf("\nWhat is the second number of your operation? \n\n");
+            
+            scanf("%lf", &secondNumber); {
+                
+                if (a == 1) {
+                    double result = firstNumber + secondNumber;
+                    printf("\nThe sum is %lf\n\n", result);
                 }
-        
-                while (anotherCalc == 1) {{
-                    
-                        printf("\nWhich operation would you like to perform? Type 1 for addition, type 2 for subtraction, type 3 for multiplication, or type 4 for division: \n\n");
-                        // this prompts user to select operation
-                        
-                        int a = -1;
-                        int error = -1;
-                        
-                        while (a < 0 || a >= 5) {
-                            fpurge(stdin);
-                            error = scanf("%d", &a);
-                            if (error != 1 || a < 0 || a >= 5 ) {
-                                printf("please enter 1, 2, 3, or 4: \n\n");
-                                a = -1;
-                            }
-                            
-                            printf("\nWhat is the first number of your operation? \n\n");
-                            // this asks for the number to which you are adding, from which you are subtracting, or which you are multiplying
-                            
-                            double firstNumber = 7;
-                            double secondNumber = 8;
-                            
-                            scanf("%lf", &firstNumber);
-                            printf("\nWhat is the second number of your operation? \n\n");
-                            // this asks for the number to be added to, subtracted from, or multiplied by the first number
-                            
-                            scanf("%lf", &secondNumber); {
-                                
-                                if (a == 1) {
-                                    double result = firstNumber + secondNumber;
-                                    printf("\nThe sum is %lf\n\n", result);
-                                }
-                                
-                                if (a == 2) {
-                                    double result = firstNumber - secondNumber;
-                                    printf("\nThe difference is %lf\n\n", result);
-                                }
-                                
-                                if (a == 3) {
-                                    double result = firstNumber * secondNumber;
-                                    printf("\nThe product is %lf\n\n", result);
-                                }
-                                
-                                if (a == 4) {
-                                    if (secondNumber == 0) {
-                                        printf("\nI'm sorry, but you cannot divide by zero!\n\n");}
-                                else
-                                {
-                                    double result = firstNumber / secondNumber;
-                                    printf("\nThe quotient is %lf\n\n", result);}
-                                }
-                            }
-                            printf("Would you like DAN'S COMMAND LINE CALCULATOR to perform another calculation?(1=Y/2=N)\n\n");
-                            scanf("%df", &anotherCalc);}
-                                                }}}}
+                
+                if (a == 2) {
+                    double result = firstNumber - secondNumber;
+                    printf("\nThe difference is %lf\n\n", result);
+                }
+                
+                if (a == 3) {
+                    double result = firstNumber * secondNumber;
+                    printf("\nThe product is %lf\n\n", result);
+                }
+                
+                if (a == 4) {
+                    if (secondNumber == 0) {
+                        printf("\nI'm sorry, but you cannot divide by zero!\n\n");
+                    } else {
+                        double result = firstNumber / secondNumber;
+                        printf("\nThe quotient is %lf\n\n", result);}
+                }
+            }
+            printf("Would you like DAN'S COMMAND LINE CALCULATOR to perform another calculation?(1=Y/2=N)\n\n");
+            scanf("%df", &anotherCalc);}
+    }
     printf("\nGoodbye!\n\n");
     
-        return 0;}
+    return 0;
+}
 
